b0dd8935cb86d7f5564d3ed8e5b1eab87ed4c88a
John Prado <jcprado20@icloud.com>
2023-04-14 18:32:30 -0400

    adds to dbconfig

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,23 +1,14 @@
-const pgp = require("pg-promise")();
-require("dotenv").config();
+const pgp = require('pg-promise')();
 
-const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER } = process.env;
+require('dotenv').config();
 
-const cn = DATABASE_URL
-  ? {
-      connectionString: DATABASE_URL,
-      max: 30,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    }
-  : {
-      host: PG_HOST,
-      port: PG_PORT,
-      database: PG_DATABASE,
-      user: PG_USER,
-    };
+const databaseUrl = process.env.DB_URL;
 
+const cn = {
+  connectionString: databaseUrl,
+  allowExitOnIdle: true,
+  max: 30,
+};
 
 const db = pgp(cn);
 
